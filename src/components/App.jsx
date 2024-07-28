e0bc9223f13fb8a56350785ad4b4c783053d6619
II-777 <45958058+II-777@users.noreply.github.com>
2024-07-28 19:39:01 -0400

    add step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,27 @@
-// import user from '../../src/data/user.json';
-import Message from './Message/Message';
-import './Message/Message.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [values, setValues] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const updateGood = () => setValues({ ...values, good: values.good + 1 });
+  const updateNeutral = () => setValues({ ...values, neutral: values.neutral + 1 });
+  const updateBad = () => setValues({ ...values, bad: values.bad + 1 });
+
+
   return (
-    <>
-      <Message name="II-777"/>
-    </>
-  )
-}
+    <div>
+      <h1>Sip Happens Café</h1>
+      <p>Please leave your feedback about our service by selecting one of the options below.</p>
+      <button onClick={updateGood}>Good</button>
+      <button onClick={updateNeutral}>Neutral</button>
+      <button onClick={updateBad}>Bad</button>
+      <p>good: {values.good}</p>
+      <p>neutral: {values.neutral}</p>
+      <p>bad: {values.bad}</p>
+      <p>total: {values.good + values.neutral + values.bad}</p>
+      <p>positive: </p>
+    </div>
+  );
+};
 
 export default App
