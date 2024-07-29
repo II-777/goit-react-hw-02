716b42cf4b38ca1dc169aa1bf453cf502f9ad37b
II-777 <45958058+II-777@users.noreply.github.com>
2024-07-28 20:43:05 -0400

    add step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,41 @@
 import { useState } from "react";
+import Options from "./Options/Options";
+import './Options/Options.css';
 
 const App = () => {
   const [values, setValues] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  const updateGood = () => setValues({ ...values, good: values.good + 1 });
-  const updateNeutral = () => setValues({ ...values, neutral: values.neutral + 1 });
-  const updateBad = () => setValues({ ...values, bad: values.bad + 1 });
+  const updateFeedback = (feedbackType) => {
+    setValues((prevValues) => ({
+      ...prevValues, [feedbackType]: prevValues[feedbackType] + 1,
+    }));
+  };
 
+  const totalFeedback = values.good + values.neutral + values.bad;
 
   return (
     <div>
       <h1>Sip Happens Café</h1>
       <p>Please leave your feedback about our service by selecting one of the options below.</p>
-      <button onClick={updateGood}>Good</button>
-      <button onClick={updateNeutral}>Neutral</button>
-      <button onClick={updateBad}>Bad</button>
-      <p>good: {values.good}</p>
-      <p>neutral: {values.neutral}</p>
-      <p>bad: {values.bad}</p>
-      <p>total: {values.good + values.neutral + values.bad}</p>
-      <p>positive: </p>
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        resetFeedback={() => setValues({ good: 0, neutral: 0, bad: 0 })}
+      />
+      {totalFeedback == 0 && (
+        <p>No feedback yet.</p>
+      )}
+      {totalFeedback > 0 && (
+        <ul>
+          <li>good: {values.good}</li>
+          <li>neutral: {values.neutral}</li>
+          <li>bad: {values.bad}</li>
+          <li>total: {totalFeedback}</li>
+          <li>positive: {Math.round((values.good / totalFeedback) * 100)}%</li>
+        </ul>
+      )}
     </div>
   );
 };
 
-export default App
+export default App;
