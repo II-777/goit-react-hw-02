19d940b57bd6a3075b5d71a03c2baf95ee1e3344
II-777 <45958058+II-777@users.noreply.github.com>
2024-07-28 21:05:06 -0400

    add step 3, 4, 5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Options from "./Options/Options";
+import Notification from "./Notification/Notification";
 import './Options/Options.css';
+import './Notification/Notification.css';
 
 const App = () => {
   const [values, setValues] = useState({ good: 0, neutral: 0, bad: 0 });
@@ -14,26 +16,34 @@ const App = () => {
   const totalFeedback = values.good + values.neutral + values.bad;
 
   return (
-    <div>
-      <h1>Sip Happens Café</h1>
-      <p>Please leave your feedback about our service by selecting one of the options below.</p>
-      <Options
-        updateFeedback={updateFeedback}
-        totalFeedback={totalFeedback}
-        resetFeedback={() => setValues({ good: 0, neutral: 0, bad: 0 })}
-      />
-      {totalFeedback == 0 && (
-        <p>No feedback yet.</p>
-      )}
-      {totalFeedback > 0 && (
-        <ul>
-          <li>good: {values.good}</li>
-          <li>neutral: {values.neutral}</li>
-          <li>bad: {values.bad}</li>
-          <li>total: {totalFeedback}</li>
-          <li>positive: {Math.round((values.good / totalFeedback) * 100)}%</li>
-        </ul>
-      )}
+    <div className='app'>
+      <div className='description'>
+        <h1>Sip Happens Café</h1>
+        <p>Please leave your feedback about our service by selecting one of the options below.</p>
+      </div>
+      <div className='options'>
+        <Options
+          updateFeedback={updateFeedback}
+          totalFeedback={totalFeedback}
+          resetFeedback={() => setValues({ good: 0, neutral: 0, bad: 0 })}
+        />
+      </div>
+      <div className='notification'>
+        {totalFeedback == 0 && (
+          <Notification />
+        )}
+      </div>
+      <div className='stats'>
+        {totalFeedback > 0 && (
+          <ul>
+            <li>good: {values.good}</li>
+            <li>neutral: {values.neutral}</li>
+            <li>bad: {values.bad}</li>
+            <li>total: {totalFeedback}</li>
+            <li>positive: {Math.round((values.good / totalFeedback) * 100)}%</li>
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,11 @@
+import './Notification.css';
+
+function Notification() {
+    return (
+        <>
+            <p>No feedback yet.</p>
+        </>
+    );
+}
+
+export default Notification;
